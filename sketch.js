f0264a8646c6ba3a5e88468b58bbed06c6cf38ca
Djimenezcas <161150226+Djimenezcas@users.noreply.github.com>
2024-06-14 19:17:50 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,10 +3,12 @@ function setup() {
 }
 
 function draw() {
-  background(20 , 75 , 200);
-circle(160, 120, 180);
-  circle(120, 120, 20)
-  circle(180, 120, 20)
+  background(200 , 0 , 0);
+circle(200, 120, 180);
+  circle(150, 100, 50)
+  circle(140, 100, 20)
+  circle(110, 100, 50)
+  circle(100, 100, 20)
   line(100, 150, 200, 150);
-  arc(150, 100, 20, 100, 0, HALF_PI);
+  arc(150, 100, 20, 100, 0);
   }
